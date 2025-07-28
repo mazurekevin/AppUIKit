59fd2969ac7da2592f29636d37e67393a5f1bc53
mazurekevin <kevin.mazure.km@gmail.com>
2025-07-28 20:16:02 +0200

    Initial Commit

---
+++ AppUIKitTests/AppUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppUIKitTests.swift
+//  AppUIKitTests
+//
+//  Created by teamdoc on 28/07/2025.
+//
+
+import Testing
+@testable import AppUIKit
+
+struct AppUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
