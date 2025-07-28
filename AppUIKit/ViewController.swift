598d280ec0c2820b4bb8e0fb13594c3f3a00a410
mazurekevin <kevin.mazure.km@gmail.com>
2025-07-28 20:27:14 +0200

    FirstCommit

--- AppUIKit/ViewController.swift
+++ AppUIKit/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button1: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    @IBAction func button1Action(_ sender: Any) {
+        print("Coucou")
+        label.text = "coucou"
+    }
+    
 
 }
 
